19cd31891945a99825bd3e59d541fb1a6369c0f9
Tiarethorns <tcarolinaespinoza@gmail.com>
2022-08-01 11:42:36 -0400

    OH DATA IMPORT

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import App from './components/App.js';
+//import App from './components/App.js';
 
-document.getElementById('root').appendChild(App());
+//document.getElementById('root').appendChild(App());
 
-function myFunction() {
+/*function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
     } else {
       x.className = "topnav";
     }
-  }
+  }*/
