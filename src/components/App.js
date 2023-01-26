642632e315a8e425e9e5112edacd3beda48834be
LissetteQ <lissetteq123@gmail.com>
2023-01-26 20:12:26 -0300

    cambios en CSS y se agrego segunda carta

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,56 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
-//
-// O alternativamente podríamos cargar el JSON de forma asíncrona usando
-// `fetch` en el momento que consideremos necesario.
-//
-// fetch('./data/pokemon/pokemon.json')
-//   .then(resp => resp.json())
-//   .then(console.log)
-//   .catch(console.error);
-//
-
 const App = () => {
+  
+ 
   const el = document.createElement('div');
 
   el.className = 'App';
-  el.textContent = 'Hola mundo!';
+  el.textContent = 'Pokemon!';
 
+
+//extraemos la api json con fetch y con map extraemos data que usaremos con un parametro.
+fetch('./data/pokemon/pokemon.json')
+.then((resp)=> resp.json())
+.then((data)=>{
+console.log(data.items);
+
+data.items.map((items)=>{
+  const card1 = document.createElement("div");
+  card1.className = 'Card';
+  card1.innerHTML = `
+  <h2>${items.id}</h2>
+  <img src="${items.image}"</img>
+  `;
+  el.appendChild(card1);
+  const card2 = document.createElement("div");
+  card2.className = 'Card2';
+
+
+
+
+  card1.appendChild(card2);
+});
+
+
+
+});
+
+
+
+    
+  
+ 
+  
+  
+  
+  
+    
+  
+
+ 
+  
+  
+  
+  
   return el;
 };
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import App from './components/App.js';
 
-document.getElementById('root').appendChild(App());
+
+
+document.getElementById('box').appendChild(App());
