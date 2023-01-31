4556fba296e6a0e5d9da424755e9d9e6bf3ef7b4
Jazmin Robles <jazmasoterapeuta@gamail.com>
2023-01-31 20:35:54 -0300

    usamos CloneNode para clonar las cartas/arreglos css y html

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 const App = () => {
+  // EL ES EL DIV PADRE
   const el = document.createElement("div");
 
   el.className = "App";
@@ -8,25 +9,58 @@ const App = () => {
     .then((resp) => resp.json())
     .then((data) => {
       console.log(data.items);
-//recorremos la data con map y creamos un nuevo parametro  items donde guardamos la data
+      //recorremos la data con map y creamos un nuevo parametro  items donde guardamos la data
       data.items.map((items) => {
-        const card1 = document.createElement("div");//creamos elemetos en DOM
-        card1.className = "Card";
-        card1.innerHTML = `
-  <h2>${items.id}</h2>
+        // creamos img en DOM e insertamos nodo en html
+        const cara = document.createElement("img");
+        cara.setAttribute("src", "./img/pokeeee.jpg");
+        cara.className = "Card2";
+        el.appendChild(cara);
+
+        // funcion para girar IMG con un click
+        let rotations = 0;
+        const img = cara;
+        img.addEventListener("click", function () {
+          rotations += 180;
+          img.style.transform = `rotateY(${rotations}180deg)`;
+        });
+
+        //clonamos img
+        var cloneImg = img.cloneNode(true);
+        el.appendChild(cloneImg);
+        //rotamos div clonados
+        let girar = 0;
+        const clonI = cloneImg;
+        clonI.addEventListener("click", function () {
+          girar += 180;
+          clonI.style.transform = `rotateY(${girar}180deg)`;
+        });
+        //creamos DIV DE POKEMONES en DOM
+        const atras = document.createElement("div");
+        atras.className = "Card";
+        atras.innerHTML = `
   <img src="${items.image}"</img>
   `;
-        el.appendChild(card1);
-        const card2 = document.createElement("div");
-        card2.className = "Card2";
 
-        card1.appendChild(card2);
+        el.appendChild(atras);
 
+        //Rotamos div ATRAS
         let rotation = 0;
-        const card = card1;
-        card.addEventListener("click", function () {
+        const Card2 = atras;
+        Card2.addEventListener("click", function () {
           rotation += 180;
-          card.style.transform = `rotateY(${rotation}180deg)`;
+          Card2.style.transform = `rotateY(${rotation}180deg)`;
+        });
+
+        //clonamos div ATRAS
+        var cloneDiv = atras.cloneNode(true);
+        el.appendChild(cloneDiv);
+        //rotamos div  ATRAS clonado
+        let rotar = 0;
+        const clon = cloneDiv;
+        clon.addEventListener("click", function () {
+          rotar += 180;
+          clon.style.transform = `rotateY(${rotar}180deg)`;
         });
       });
     });
--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
 import App from "./components/App.js";
 
-document.getElementById("root").appendChild(App(darVuelta));
-
-
-function darVuelta(){
-
-}
+document.getElementById("root").appendChild(App());
